6a6163b4e016250a7c0adee649c6db691d73555d
Furkan KART <planlama11@berteks.com>
2022-04-05 11:27:33 +0300

    Version 1.1

---
+++ LastPhotosSave/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LastPhotosSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonGozat = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxPath.Location = new System.Drawing.Point(12, 12);
+            this.textBoxPath.Multiline = true;
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(402, 24);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // buttonGozat
+            // 
+            this.buttonGozat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonGozat.Location = new System.Drawing.Point(12, 42);
+            this.buttonGozat.Name = "buttonGozat";
+            this.buttonGozat.Size = new System.Drawing.Size(402, 64);
+            this.buttonGozat.TabIndex = 1;
+            this.buttonGozat.Text = "Kaydedilecek Konumu Belirle";
+            this.buttonGozat.UseVisualStyleBackColor = true;
+            this.buttonGozat.Click += new System.EventHandler(this.buttonGozat_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSave.Location = new System.Drawing.Point(12, 112);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(402, 64);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Kaydet";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 185);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonGozat);
+            this.Controls.Add(this.textBoxPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Berteks Fotoğraflar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonGozat;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ LastPhotosSave/Form1.cs
@@ -0,0 +1,90 @@
+﻿using MediaDevices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LastPhotosSave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGozat_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    textBoxPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            var devices = MediaDevice.GetDevices();
+            using (var device = devices.First(d => d.FriendlyName == "Apple iPhone"))
+            {
+                device.Connect();
+                var photoDir = device.GetDirectoryInfo(@"\Dahili olarak paylaşılan depolama alanı\DCIM\Camera");
+                var videoDir = device.GetDirectoryInfo(@"\Dahili olarak paylaşılan depolama alanı\DCIM\Camera");
+
+                var files = photoDir.EnumerateFiles("*.jpg", SearchOption.AllDirectories);
+                var fileVideos = photoDir.EnumerateFiles("*.mp4", SearchOption.AllDirectories);
+
+                foreach (var file in files)
+                {
+                    MemoryStream memoryStream = new System.IO.MemoryStream();
+                    device.DownloadFile(file.FullName, memoryStream);
+                    memoryStream.Position = 0;
+                    WriteSreamToDisk($@"{textBoxPath.Text}/{file.Name}", memoryStream);
+                    device.DeleteFile(file.FullName);
+                }
+                foreach (var fileVideo in fileVideos)
+                {
+                    MemoryStream memoryStream = new System.IO.MemoryStream();
+                    device.DownloadFile(fileVideo.FullName, memoryStream);
+                    memoryStream.Position = 0;
+                    WriteSreamToDiskVideo($@"{textBoxPath.Text}/{fileVideo.Name}", memoryStream);
+                    device.DeleteFile(fileVideo.FullName);
+                }
+                device.Disconnect();
+            }
+            textBoxPath.Text = "";
+            MessageBox.Show("Fotoğraflar başarılı bir şekilde aktarıldı.");
+        }
+
+        static void WriteSreamToDisk(string filePath, MemoryStream memoryStream)
+        {
+            using (FileStream file = new FileStream(filePath, FileMode.Create, System.IO.FileAccess.Write))
+            {
+                byte[] bytes = new byte[memoryStream.Length];
+                memoryStream.Read(bytes, 0, (int)memoryStream.Length);
+                file.Write(bytes, 0, bytes.Length);
+                memoryStream.Close();
+            }
+        }
+        static void WriteSreamToDiskVideo(string filePath, MemoryStream memoryStream)
+        {
+            using (FileStream file = new FileStream(filePath, FileMode.Create, System.IO.FileAccess.Write))
+            {
+                byte[] bytes = new byte[memoryStream.Length];
+                memoryStream.Read(bytes, 0, (int)memoryStream.Length);
+                file.Write(bytes, 0, bytes.Length);
+                memoryStream.Close();
+            }
+        }
+    }
+}
